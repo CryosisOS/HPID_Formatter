bb1a4279da9983e6ecac15d26c31986ddc3b49cd
CryosisOS <nathanvdvelde@gmail.com>
2020-01-16 15:39:36 +0800

    Initial Commit - Should be a finished tool

---
+++ run.py
@@ -0,0 +1,33 @@
+import sys
+import ast
+import json
+
+def recurse(d, allStrings, currentString=list()):
+    for k, v in d.items():
+        if isinstance(v, dict):
+            currentString.append(k)
+            recurse(v, allStrings, currentString=currentString)
+            currentString.pop()
+        elif isinstance(v, list):
+            currentString.append(k+" - "+str(v))
+            allStrings.append(currentString.copy())
+            currentString.pop()
+        else:   
+            currentString.append(k+" - "+v)
+            allStrings.append(currentString.copy())
+            currentString.pop()
+    return allStrings.copy()
+
+
+with open(sys.argv[1]) as json_file:
+    data = json.load(json_file)
+    strings = list()
+    strings = recurse(data, strings)
+formatted_strings = list()
+for string in strings:
+    formatted_strings.append(" - ".join(string))
+output = open(sys.argv[2], "w")
+for string in formatted_strings:
+    output.write("=>"+string+"\n")
+output.close()
+print("Format finished.")
